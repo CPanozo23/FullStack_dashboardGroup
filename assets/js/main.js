fe3422ac8cbe35696c6d027fce22db51ac01eb67
CPanozo23 <cpanozo@outlook.com>
2023-05-20 10:45:56 -0400

    General dashboard structure

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+import { onload } from "./onload.js"
+import { mainBalance } from "./bankInfo/mainBalance.js"
+import { mainBankInfo } from "./bankInfo/mainBankInfo.js"
+import { mainIndicator } from "./bankInfo/mainIndicator.js"
