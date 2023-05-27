8f3fabcc4ed46db321c8a28c2d69caa5964d6f22
Jonathan Hernandez <jonathan.a.hernandez.m@gmail.com>
2023-05-27 13:28:17 -0400

    corrige error carga valores UF y UTM

--- assets/js/indicators/formatData.js
+++ assets/js/indicators/formatData.js
@@ -1,7 +1,34 @@
-export const formatData = (datos) =>{
-    const labels = datos.map((elemento) => elemento.Fecha)
-    const data = datos.map((elemento) => parseFloat(elemento.Valor.replace(",",".")))
-    // console.log(labels, data)
+export const formatData = (datos, indicador) =>{
+    const labels = datos.map((elemento) => {
+        const [yyyy,mm,dd] = elemento.Fecha.split('-');
+        return `${dd}-${mm}-${yyyy}`
+        //elemento.Fecha
+    });
+    let data =""
+    switch (indicador) {
+        case 'ipc':
+            data = datos.map((elemento) => parseFloat(elemento.Valor.replace(",",".")));
+            break;
+        case 'dolar':
+            data = datos.map((elemento) => parseFloat(elemento.Valor.replace(",",".")));
+            break;
+        case 'euro':
+            data = datos.map((elemento) => parseFloat(elemento.Valor.replace(",",".")));
+            break;
+        case 'tip':   
+            data = datos.map((elemento) => parseFloat(elemento.Valor.replace(",",".")));
+            break;
+        case 'tmc':
+            data = datos.map((elemento) => parseFloat(elemento.Valor.replace(",",".")));
+            break;
+        case 'uf':
+            data = datos.map((elemento) => parseFloat(elemento.Valor.replace(".","")));
+            break;
+        case 'utm':
+            data = datos.map((elemento) => parseFloat(elemento.Valor.replace(".","")));
+            break;
+        }
+   // console.log(labels, data)
     return {labels, data}    
 }
     // const labels = datos.serie.map((elemento) => elemento.fecha.split('T')[0])
